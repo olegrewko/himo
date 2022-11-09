cd69aec3b2911dd75668069e660e30a8de40ea64
olegrewko <olegrewko@gmail.com>
2022-11-09 22:58:47 +0300

    first commit

---
+++ docs/js/main.js
@@ -0,0 +1,24 @@
+
+      $(document).ready(function(){
+        
+      $('.bike-slider').slick({
+        arrows: false,
+        dots: true,
+        fade: true,
+        autoplay: true,
+        autoplaySpeed: 2000
+
+    });
+    // slider02
+      $('.slider').slick({
+        arrows: false,
+        dots: true,
+        fade: true,
+        autoplay: true,
+        autoplaySpeed: 2000
+
+    });
+  
+          
+
+    });
